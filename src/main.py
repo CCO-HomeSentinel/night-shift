a42a4922d38c87a97f1d6bf1886c3c0a150810f8
LeoVasc5 <leovasc5@hotmail.com>
2024-05-28 21:59:35 -0300

    Estruturado aplicação

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import os
-from src.config.logger import Logger
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-from connection.MySQLConnection import MySQLConnection
-from service.process import process
+import threading
 from functools import partial
+from src.config.logger import Logger
+from connection.MySQLConnection import MySQLConnection
+from service.process import initialize_spark, process
 
 load_dotenv()
 
@@ -11,9 +13,11 @@ ENABLE_LOGS = os.getenv("ENABLE_LOGS").lower() == "true"
 
 if ENABLE_LOGS:
     logger = Logger()
+else:
+    logger = None
 
-if os.getenv("INTERVALO_BACKUP") is None:
-    INTERVALO_BACKUP = 1
+INTERVALO_BACKUP = int(os.getenv("INTERVALO_BACKUP", 1))
+
 
 def set_up_database():
     mysql_connection = MySQLConnection()
@@ -21,11 +25,49 @@ def set_up_database():
     relacoes = mysql_connection.mapper_query()
     session.close()
 
-    return partial(process, relacoes)
+    return relacoes
+
+
+def set_up_spark():
+    spark = initialize_spark(logger)
+    return spark
+
+
+def print_name(name):
+    if logger:
+        logger.info(f"Received name: {name}")
+    else:
+        print(f"Received name: {name}")
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/file', methods=['POST'])
+def handle_file():
+    data = request.get_json()
+
+    if 'name' in data:
+        name = data['name']
+
+        thread = threading.Thread(target=process, args=(name, logger,))
+        thread.start()
+
+        if logger:
+            logger.info(f"Requisição recebida e em processamento. Nome: {name}")
+
+        return jsonify({"message": "Requisição recebi e em processamento."}), 202
+    else:
+        if logger:
+            logger.error("Não encontrado chave 'name' em body")
+
+        return jsonify({"error": "Não encontrado chave 'name' em body"}), 400
+
 
 def main():
-    set_up_database(logger)
-    
+    relacoes = set_up_database()
+    spark = set_up_spark()
+
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
--- src/service/process.py
+++ src/service/process.py
@@ -3,14 +3,33 @@ from pyspark.sql import SparkSession
 import numpy as np
 import pandas as pd
 
-# conf = SparkConf()
-# conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
-# conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.InstanceProfileCredentials Provider')
-# spark = SparkSession.builder.config(conf=conf).getOrCreate()
+def initialize_spark(logger):
+    logger.info("Iniciando Spark")
+    return 'objeto spark'
 
-def process(self, filename=None):
+def process(self, filename=None, logger=None):
+    if logger:
+        logger.info(f"Processando arquivo {filename}")
+
+    dados = trazer_arquivo(filename)
+    dados_reduzidos = reduzir_redundancia(dados)
+    tabularizar(dados_reduzidos)
+    criar_kpis(dados)
+    
     print("main do tratamento")
-    df = set_up(self, filename)
+
+
+def trazer_arquivo(filename):
+    print("traga o arquivo aqui")
+
 
 def reduzir_redundancia():
     print("reduza a redundancia aqui")
+
+
+def tabularizar(dados):
+    print("tabularize os dados aqui")
+
+
+def criar_kpis():
+    print("crie os kpis aqui")
